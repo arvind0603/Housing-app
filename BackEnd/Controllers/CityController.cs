141b5091178603377b277515810c1e3a8969f349
arvind-lap <arvind.kuppusamy@outlook.com>
2023-08-21 14:02:46 -0700

    Implemeted Add/Delete city api using async method

--- BackEnd/Controllers/CityController.cs
+++ BackEnd/Controllers/CityController.cs
@@ -1,6 +1,8 @@
 ﻿using BackEnd.Data;
+using BackEnd.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd.Controllers
 {
@@ -14,11 +16,41 @@ namespace BackEnd.Controllers
             this.dataContext = dataContext;
 
         }
+        //Get api/city
         [HttpGet]
-        public IActionResult Get() {
+        public async Task<IActionResult> Get() {
 
-            var cities = dataContext.Cities.ToList();
+            var cities = await dataContext.Cities.ToListAsync();
             return Ok(cities);
+        }
+
+        //Post api/city/post (Json object)
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city) {
+            await dataContext.Cities.AddAsync(city);
+            await dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+
+        //Post api/city/add?cityName=New York (string)
+        [HttpPost("add")]
+        [HttpPost("add/{cityName}")]
+        public async Task<IActionResult> AddCity(string cityName) {
+            City city = new City();
+            city.Name = cityName;
+            await dataContext.Cities.AddAsync(city);
+            await dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id) {
+
+            var city = await dataContext.Cities.FindAsync(id); 
+            dataContext.Cities.Remove(city);
+            await dataContext.SaveChangesAsync();
+            return Ok(id);
         }
     }
 }
