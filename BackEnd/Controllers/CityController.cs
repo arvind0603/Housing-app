bf6289cee821ed9ed3fec2b435362b75c987cfe6
arvind-lap <arvind.kuppusamy@outlook.com>
2023-08-21 15:23:30 -0700

    Decoupled EntityFramework from controller and implemeted repo pattern instead

--- BackEnd/Controllers/CityController.cs
+++ BackEnd/Controllers/CityController.cs
@@ -1,4 +1,5 @@
 ﻿using BackEnd.Data;
+using BackEnd.Data.Repo;
 using BackEnd.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,46 +11,47 @@ namespace BackEnd.Controllers
     [ApiController]
     public class CityController : ControllerBase
     {
-        private readonly DataContext dataContext;
-        public CityController(DataContext dataContext)
+        // private readonly DataContext dataContext;
+        public ICityRepository repo;
+        public CityController(ICityRepository repo)
         {
-            this.dataContext = dataContext;
+            this.repo = repo;
+            // this.dataContext = dataContext;
 
         }
         //Get api/city
         [HttpGet]
         public async Task<IActionResult> Get() {
 
-            var cities = await dataContext.Cities.ToListAsync();
+            var cities = await repo.GetCitiesAsync();
             return Ok(cities);
         }
 
         //Post api/city/post (Json object)
         [HttpPost("post")]
         public async Task<IActionResult> AddCity(City city) {
-            await dataContext.Cities.AddAsync(city);
-            await dataContext.SaveChangesAsync();
-            return Ok(city);
+            repo.AddCity(city);
+            await repo.SaveChangesAsync();
+            return StatusCode(201);
         }
 
 
-        //Post api/city/add?cityName=New York (string)
-        [HttpPost("add")]
-        [HttpPost("add/{cityName}")]
-        public async Task<IActionResult> AddCity(string cityName) {
-            City city = new City();
-            city.Name = cityName;
-            await dataContext.Cities.AddAsync(city);
-            await dataContext.SaveChangesAsync();
-            return Ok(city);
-        }
+        // //Post api/city/add?cityName=New York (string)
+        // [HttpPost("add")]
+        // [HttpPost("add/{cityName}")]
+        // public async Task<IActionResult> AddCity(string cityName) {
+        //     City city = new City();
+        //     city.Name = cityName;
+        //     await dataContext.Cities.AddAsync(city);
+        //     await dataContext.SaveChangesAsync();
+        //     return Ok(city);
+        // }
 
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeleteCity(int id) {
 
-            var city = await dataContext.Cities.FindAsync(id); 
-            dataContext.Cities.Remove(city);
-            await dataContext.SaveChangesAsync();
+            repo.DeleteCity(id);
+            await repo.SaveChangesAsync();
             return Ok(id);
         }
     }
