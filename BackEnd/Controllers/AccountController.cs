092cf6b6aaa16a78bff8f6b1a05a97fa2ad58b33
arvind-lap <arvind.kuppusamy@outlook.com>
2023-08-23 00:58:07 -0700

    Created JWT token

--- BackEnd/Controllers/AccountController.cs
+++ BackEnd/Controllers/AccountController.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using BackEnd.Dtos;
 using BackEnd.Interfaces;
 using BackEnd.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BackEnd.Controllers
 {
@@ -30,9 +34,31 @@ namespace BackEnd.Controllers
 
             var loginRes = new LoginResDto();
             loginRes.Username = loginReq.Username;
-            loginRes.Token = "Yet to generate Token";
+            loginRes.Token = CreateJWT(user);
 
             return Ok(loginRes);
+        }
+
+        private string CreateJWT(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Shhh... this is Top Secret Key"));
+
+            var claims = new Claim[] {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescription = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = signingCredentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
