648cc385e5f28d8a2208dc67c662758524970532
arvind-lap <arvind.kuppusamy@outlook.com>
2023-08-24 19:15:17 -0700

    Implemeted retry mechanism for failed requests

--- FrontEnd/src/app/services/httperror-interceptor.service.ts
+++ FrontEnd/src/app/services/httperror-interceptor.service.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable, catchError, throwError } from "rxjs";
+import { Observable, catchError, concat, concatMap, of, retry, retryWhen, throwError } from "rxjs";
 import { AlertifyService } from "./alertify.service";
 import { Injectable } from "@angular/core";
+import { ErrorCode } from "../enums/enums";
 
 
 @Injectable({
@@ -16,6 +17,7 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
     console.log('HTTP Request Started');
     return next.handle(req)
       .pipe(
+        retryWhen(error => this.retryRequest(error, 10)),
         catchError((error: HttpErrorResponse) => {
           const errorMessage = this.setError(error)
           console.log(error);
@@ -25,6 +27,24 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
       );
   }
 
+  //Retry the request in case of Error
+  retryRequest(error: Observable<any>, retryCount: number): Observable<any> {
+    return error.pipe(
+      concatMap((checkErr: HttpErrorResponse, count: number) => {
+
+        if (count <= retryCount) {
+          switch (checkErr.status) {
+            case ErrorCode.serverDown:
+              return of(checkErr)
+            case ErrorCode.unauthorized:
+              return of(checkErr)
+          }
+        }
+        return throwError(checkErr);
+      })
+    )
+  }
+
   setError(error: HttpErrorResponse): string {
     let errorMessage = 'Unknown Error Occured';
     if (error.error instanceof ErrorEvent) {
@@ -32,7 +52,7 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
       errorMessage = error.error.message;
     } else {
       //server side error
-      if (error.status !== 0) {
+      if (error.status !== ErrorCode.serverDown) {
         errorMessage = error.error.errorMessage;
       }
     }
